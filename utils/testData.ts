1aa1cd865f11700bf4e1909631648f92b08d63ac
Balaji <mgbalaji89@gmail.com>
2026-09-23 12:12:34 +0530

    Create testData.ts

---
+++ utils/testData.ts
@@ -0,0 +1,12 @@
+export const testData = {
+	username: 'balaji_test_01',
+	password: 'Test@123',
+	order: {
+		name: 'Balaji',
+		country: 'India',
+		city: 'Bangalore',
+		card: '1234567890123456',
+		month: '12',
+		year: '2027'
+	}
+};
