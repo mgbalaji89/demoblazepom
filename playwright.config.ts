16c2691fee31bf1f2e7bbee047d2c8d5fbbcf99b
Balaji <mgbalaji89@gmail.com>
2026-09-23 12:24:28 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,18 @@
-// playwright.config.ts
-
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
 
+  reporter: [
+    ['html'],
+    ['list']
+  ],
+
   use: {
     browserName: 'chromium',
-    headless: false,
-    trace: 'on'
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure'
   }
 });
